1b25de9dde353494d0938b459f59e6d0ab1e215e
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-01 19:24:06 -1000

    fist solution

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
-  
+  let myObject = {};
+  return {
+    setValue: function(key, value){
+      if (!key || !value){
+        return null;
+      }
+      myObject.key = value;
+      //Object.defineProperty(myObject, key.
+    },
+    getValue: function(key){
+      if (key === undefined){
+        return null;
+      }
+      //if ()
+      if (!myObject.key){
+        return null;
+      }
+      return myObject.key;
+    }
+  }
 };
